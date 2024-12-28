b193199bb1a7a6639d6f834f24d2f8d358ee9d46
unknown <kottugoroshko232@gmail.com>
2024-12-28 15:35:48 +0200

    added new veiw for event

--- booking/urls.py
+++ booking/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("rooms-list/", views.room_list, name="rooms-list"),
     path("book-room/", views.book_room, name="book-room"),
     path("booking-details/<int:pk>/", views.booking_details, name="booking-details"),
-
+    path("book-event/", views.book_event, name="book-event"),
+    path("event-details/<int:pk>/", views.event_details, name="event-details"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from booking.models import Room, Booking
+from booking.models import Room, Booking, Event
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -7,6 +7,19 @@ def index(request):
         "render_string":"Hello, world"
     }
     return render(request, template_name="booking/index.html", context=context)
+
+def check_rooms():
+    rooms = Room.objects.all()
+    return {"rooms":rooms}
+
+def check_event_rooms():
+    rooms = Room.objects.all()
+    rooms_new=[]
+    for room in rooms:
+        if room.capacity>10:
+            rooms_new.append(room)
+        
+    return {"rooms":rooms_new}
 
 def room_list(request):
     rooms = Room.objects.all()
@@ -22,7 +35,7 @@ def book_room(request):
         end_time = request.POST.get("end-time")
 
         try:
-            room = Room.objects.get(number = room_number)
+            room = Room.objects.get(id = room_number)
         except ValueError:
             return HttpResponse(
                 content="Wrong value for room number",
@@ -41,7 +54,7 @@ def book_room(request):
         )
         return redirect ("booking-details", pk=booking.id)
     else:
-        return render(request, template_name="booking/booking_form.html")
+        return render(request, template_name="booking/booking_form.html", context=check_rooms())
 
 def booking_details(request, pk):
     try:
@@ -55,5 +68,52 @@ def booking_details(request, pk):
             content="This booking doesn't exist",
             status = 404
         )
+    
 
+def book_event(request):
 
+    if request.method == "POST":
+        event_title = request.POST.get("event-title")
+        room_number = request.POST.get("room-number")
+        print(room_number)
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            room = Room.objects.get(id = room_number)
+        except ValueError:
+            return HttpResponse(
+                content="Wrong value for room number",
+                status=400
+            )
+        except Room.DoesNotExist:
+            return HttpResponse(
+                content="This room number doesn't exists",
+                status = 404
+            )
+        booking = Booking.objects.create(
+            user = request.user,
+            room = room,
+            start_time = start_time,
+            end_time = end_time
+        )
+        event = Event.objects.create(
+            title = event_title,
+            booking = booking
+        )
+        return redirect ("event-details", pk=event.id)
+    else:
+        return render(request, template_name="booking/booking_event.html", context=check_event_rooms())
+
+def event_details(request, pk):
+    try:
+        event = Event.objects.get(id = pk)
+        context = {
+            "event":event,
+        }
+        return render(request=request, template_name="booking/event_details.html", context=context)
+    except Event.DoesNotExist:
+        return HttpResponse(
+            content="This event doesn't exist",
+            status = 404
+        )
